964ebaf3a7c7989e231ed573073a51beca0e49c8
am-camila <marchinicamila94@gmail.com>
2021-01-16 23:27:28 -0300

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,19 +3,24 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
 
   function handlePersonChange(ev){
     setNewName(ev.target.value)
   }
+  function handleNumberChange(ev){
+    setNewNumber(ev.target.value)
+  }
 
   function addPerson(ev){
     ev.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if (persons.find(p=>p.name.toLowerCase()===newPerson.name.toLowerCase())){
@@ -24,6 +29,7 @@ const App = () => {
     setPersons(persons.concat(newPerson))
     }
     setNewName('')
+    setNewNumber('')
   }
 
 
@@ -31,17 +37,14 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input type="text" value={newName} onChange={handlePersonChange}/></div>
+        <div>number: <input type="text" value={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       
       <h2>Numbers</h2>
       <ul>
-        {persons.map(p => <li key={p.name}>{p.name}</li>)}
+        {persons.map(p => <li key={p.name}>{p.name} {p.number}</li>)}
       </ul>
     
     </div>
